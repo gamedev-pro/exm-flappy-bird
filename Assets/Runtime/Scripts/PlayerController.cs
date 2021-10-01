fa3d9f2da46c372f9f147dfc2bf639068f572bda
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-10-01 06:20:10 -0600

    Remove rigidbody + tweaks

--- Assets/Runtime/Scripts/PlayerController.cs
+++ Assets/Runtime/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float forwardSpeed = 10;
     [SerializeField] private float flapVelocity = 10;
+
+    [SerializeField] private float gravity = 1.8f * 9.8f;
 
     [SerializeField]
     [Range(0, 180)]
@@ -14,49 +16,58 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float rotateDownSpeed = 5;
 
-    private Rigidbody2D rb;
+    private Vector3 velocity;
+    private float zRot;
 
     private PlayerInput input;
 
-    private Vector3 targetVelocity;
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         input = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 velocity = MoveForward(rb.velocity);
-        float zRot = RotateDown(transform.rotation.eulerAngles.z);
+        ModifyVelocity();
+        RotateDown();
+        ProcessInput();
 
+        transform.rotation = Quaternion.Euler(Vector3.forward * zRot);
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    private float ProcessInput()
+    {
         if (input.TapUp())
         {
             velocity.y = flapVelocity;
             zRot = flapAngleDegress;
         }
 
-        rb.velocity = velocity;
-        transform.rotation = Quaternion.Euler(Vector3.forward * zRot);
+        return zRot;
+    }
+
+    private void ModifyVelocity()
+    {
+        velocity.x = forwardSpeed;
+        velocity.y -= gravity * Time.deltaTime;
+    }
+
+    private void RotateDown()
+    {
+        if (velocity.y < 0)
+        {
+            zRot -= rotateDownSpeed * Time.deltaTime;
+            zRot = Mathf.Max(-90, zRot);
+        }
     }
 
     public void Die()
     {
         forwardSpeed = 0;
-        rb.velocity = Vector2.zero;
+        velocity = Vector3.zero;
         GetComponent<BoxCollider2D>().enabled = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    private Vector2 MoveForward(Vector2 velocity)
-    {
-        velocity.x = forwardSpeed;
-        return velocity;
-    }
-
-    private float RotateDown(float zRot)
-    {
-        return Mathf.Max(-90, zRot - rotateDownSpeed * Time.deltaTime);
     }
 }
